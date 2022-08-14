2bdfac0e923f958093762bda693648ef9f72c273
Nisschhal <mrnischalpuri@gmail.com>
2022-08-14 15:07:52 +0545

    creating random colors

---
+++ random-color-generator/index.js
@@ -0,0 +1,13 @@
+const container = document.querySelectorAll('.container');
+
+
+for (let i =0 ;i < 30; i++){
+    const newColorContainerEl = document.createElement('div'); //creating the new element of div
+    newColorContainerEl.classList.add('color-container'); // adding the 'color-container' class into the created element
+    container.appendChild(newColorEl); // adding the new div element with its 'color-container' class into the container parent
+
+} // creating 30 newColorContainers 
+
+
+
+
