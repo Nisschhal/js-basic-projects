78350d54aeb3ef51c975ab4031c08e045c634bfa
Nisschhal <mrnischalpuri@gmail.com>
2022-08-15 16:22:05 +0545

    random-image generator compelete

---
+++ random-picture-generator/index.js
@@ -0,0 +1,19 @@
+const imageContainerEl = document.querySelector('.image-container');
+
+
+const btnEl = document.querySelector('.btn');
+
+
+btnEl.addEventListener('click', ()=> {
+    imageNum = 10;
+    addNewImage();
+})
+
+const addNewImage = () => {
+    for (let i =0; i< imageNum; i++){
+        const newImage = document.createElement('img')
+        newImage.src = `https://picsum.photos/200/300?random=${Math.floor(Math.random()*2000)}`;
+        imageContainerEl.appendChild(newImage);
+        
+    }
+}
